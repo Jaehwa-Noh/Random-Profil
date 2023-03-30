3c6437831c33d42ed3265709b47d1572b8d8968f
shwoghk14 <shwoghk14@gmail.com>
2023-03-30 16:15:50 +0900

    Make Tab Items with line.

--- Random Profil/View/Profile/ProfilePagerView.swift
+++ Random Profil/View/Profile/ProfilePagerView.swift
@@ -8,8 +8,42 @@
 import SwiftUI
 
 struct ProfilePagerView: View {
+    private var tabList = ["남자", "여자"]
+    @State private var selectedTab: Int = 0
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            HStack {
+                Spacer()
+                ForEach(Array(tabList.enumerated()), id: \.offset) {
+                    index, tabTitle in
+                    Button {
+                        selectedTab = index
+                    } label: {
+                        GeometryReader { geo in
+                            VStack {
+                                
+                                Text(tabTitle)
+                                    .foregroundColor(selectedTab == index ? .black : .gray)
+                                
+                                
+                                Rectangle()
+                                    .fill(Color.green)
+                                    .opacity(selectedTab == index ? 1 : 0)
+                                    .frame(width:geo.size.width, height: 2)
+                                
+                                Spacer()
+                                
+                            }
+                            
+                        }
+                        Spacer()
+                    }
+                }
+            }
+            Spacer()
+            
+        }
     }
 }
 
