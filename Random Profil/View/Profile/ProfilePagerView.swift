3c95c4f2a2910c54f4ea40ac8fc34aebc8fced6d
shwoghk14 <shwoghk14@gmail.com>
2023-03-30 15:46:04 +0900

    Create Profile Pager View

---
+++ Random Profil/View/Profile/ProfilePagerView.swift
@@ -0,0 +1,20 @@
+//
+//  ProfilePagerView.swift
+//  Random Profil
+//
+//  Created by Jaehwa Noh on 2023/03/30.
+//
+
+import SwiftUI
+
+struct ProfilePagerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ProfilePagerView_Previews: PreviewProvider {
+    static var previews: some View {
+        ProfilePagerView()
+    }
+}
