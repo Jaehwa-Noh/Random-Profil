64bfc50a4b2562eea002309b552d0807b5db1b4d
shwoghk14 <shwoghk14@gmail.com>
2023-03-30 15:38:41 +0900

    Initial Commit

---
+++ Random Profil/Random_ProfilApp.swift
@@ -0,0 +1,17 @@
+//
+//  Random_ProfilApp.swift
+//  Random Profil
+//
+//  Created by Jaehwa Noh on 2023/03/30.
+//
+
+import SwiftUI
+
+@main
+struct Random_ProfilApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
