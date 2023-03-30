bc102c17b9d69f127599fd6b5159725fa67fc498
shwoghk14 <shwoghk14@gmail.com>
2023-03-30 15:48:14 +0900

    Create Female View

---
+++ Random Profil/View/Profile/Female/FemaleView.swift
@@ -0,0 +1,20 @@
+//
+//  FemaleView.swift
+//  Random Profil
+//
+//  Created by Jaehwa Noh on 2023/03/30.
+//
+
+import SwiftUI
+
+struct FemaleView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FemaleView_Previews: PreviewProvider {
+    static var previews: some View {
+        FemaleView()
+    }
+}
