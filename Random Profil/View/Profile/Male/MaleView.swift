1f12b0e82a489783926f582523c8dbac61a4e658
shwoghk14 <shwoghk14@gmail.com>
2023-03-30 16:54:49 +0900

    Apply ProfileProtocol

--- Random Profil/View/Profile/Male/MaleView.swift
+++ Random Profil/View/Profile/Male/MaleView.swift
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-struct MaleView: View {
+struct MaleView: View, ProfilePage {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
