2c9eaad5e86e2bdef876815b91a7627659db981f
shwoghk14 <shwoghk14@gmail.com>
2023-03-31 14:26:57 +0900

    Create URL Class.

---
+++ Random Profil/Model/Network/RandomUserClient.swift
@@ -0,0 +1,24 @@
+//
+//  RandomUserClient.swift
+//  Random Profil
+//
+//  Created by Jaehwa Noh on 2023/03/31.
+//
+
+import Foundation
+
+class RandomUserClient {
+    enum Endpoints {
+        static let base = "https://randomuser.me/api/"
+        static let getPerson = "?results=70"
+        
+        case getPersonList(gender: String)
+        
+        var url: String {
+            switch self {
+            case .getPersonList(let gender):
+                return Endpoints.base + Endpoints.getPerson + "&gender=\(gender)"
+            }
+        }
+    }
+}
