e8c416b3060d52377f1908bcd6fdc3c7d8d96f21
shwoghk14 <shwoghk14@gmail.com>
2023-04-01 15:16:13 +0900

    Make Female View

--- Random Profil/View/Profile/FemaleView/FemaleView.swift
+++ Random Profil/View/Profile/FemaleView/FemaleView.swift
@@ -8,9 +8,46 @@
 import SwiftUI
 
 struct FemaleView: View, ProfilePage {
+    @StateObject private var viewModel = MaleFemaleViewModel(isMale: false)
+    @State var isError: Bool = false
+    private var isMale = false
+    
+    private let imageCaching: ImageCaching = ImageCaching()
+    
     var body: some View {
-        ScrollView {
-            Text("Female View")
+        ZStack {
+            if viewModel.people.isEmpty && !isError {
+                ProgressView()
+                    .scaleEffect(CGSize(width: 1.5, height: 1.5))
+            }
+            ScrollView {
+                LazyVStack {
+                    ForEach(viewModel.people, id:\.self.uuid) { people in
+                        ProfileListItemView(
+                            name: people.name,
+                            location: people.location,
+                            email: people.email,
+                            imageURL: people.imageURL,
+                            profileImage: people.profileImage,
+                            viewModel: viewModel)
+                        .onAppear {
+                            if people == viewModel.people.last {
+                                viewModel.getMoreList(isMale: isMale)
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        .refreshable {
+            isError = false
+            viewModel.getGenderList(isMale: isMale)
+        }
+        
+        .alert("서버 통신 문제", isPresented: $viewModel.isServerError) {
+            Button("확인", role:.cancel) {
+                isError = true
+            }
         }
         
     }
